ca6d8a8f66b1afd930cb8f8235d6337310c9345a
Tai Shi Ling <shiling@uilicious.com>
2023-04-26 11:43:57 +0800

    Add test file, update github actions

---
+++ tests/Login to Github.test.js
@@ -0,0 +1,15 @@
+// Navigate to github.com
+I.goTo("https://github.com")
+
+// Go to "Sign in" page
+I.click("Sign in")
+
+// Sign in
+I.fill("Username", "johndoe")
+I.fill("Password", "supersecretpassword")
+I.click("Sign in")
+
+// If logged in, expect to see "My repos"
+I.see("My repos")
+
+// Check out our docs to learn more: https://docs.uilicious.com
