2f41acfe234c9742ac9cc24125d67549ea740531
Tai Shi Ling <shiling@uilicious.com>
2023-04-26 13:41:55 +0800

    Update tests

---
+++ tests/Search for uilicious-cli.test.js
@@ -0,0 +1,11 @@
+I.goTo("https://github.com/")
+
+I.fill("Search Github", "uilicious-cli")
+
+I.pressEnter()
+
+I.see("uilicious/uilicious-cli")
+
+I.click("uilicious/uilicious-cli")
+
+I.amAt("https://github.com/uilicious/uilicious-cli")
